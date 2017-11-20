8e03a33fb916c7d599bf25889c6a8add96513a6d
sudsy <sudsy1958@gmail.com>
2017-11-21 08:21:26 +1100

    working version

--- rankall.R
+++ rankall.R
@@ -11,7 +11,7 @@ rankall <- function(routcome, num = "best") {
   }
   
   if(num > nrow(foutcome) & (num %in% rvalues) == FALSE ) {
-      print("bad value in num")
+      #print("bad value in num")
       return(NA)
   }
   if(num == "best") {
@@ -24,47 +24,75 @@ rankall <- function(routcome, num = "best") {
   
   
   if(routcome == "heart attack") {
-    
     for(rstate in states) {
       required_state <- filter(foutcome, State == rstate)
       #print(rstate)
       fstate_data <- required_state[,c(2,11)]
         tst1 <- fstate_data[order(suppressWarnings(as.numeric(fstate_data[,2])),fstate_data[,1],na.last = NA),]
         if(num == "worst") {
-          print("setting worst")
-           num <- nrow(tst1)
-        }
-        hname <- tst1$Hospital.Name[num]
-        if(num > nrow(tst1)) {
-          print("setting to NA")
+          hname <- tst1$Hospital.Name[nrow(tst1)]
+          
+        } else if(num > nrow(tst1)) {
+          #print("setting to NA")
           hname = "<NA>"
-        } 
+        } else {
+          hname <- tst1$Hospital.Name[num]
+        }
         newrow <- data.frame(hname,rstate)
         results <- rbind(results,newrow)
+        # add to data frame
+    }
+    colnames(results) <- c("hospital", "state")
+    return(results)
+  }
+ 
+  if(routcome == "heart failure") {
+    for(rstate in states) {
+      required_state <- filter(foutcome, State == rstate)
+      #print(rstate)
+      fstate_data <- required_state[,c(2,17)]
+      tst1 <- fstate_data[order(suppressWarnings(as.numeric(fstate_data[,2])),fstate_data[,1],na.last = NA),]
+      if(num == "worst") {
+        hname <- tst1$Hospital.Name[nrow(tst1)]
+        
+      } else if(num > nrow(tst1)) {
+        #print("setting to NA")
+        hname = "<NA>"
+      } else {
+        hname <- tst1$Hospital.Name[num]
+      }
+      newrow <- data.frame(hname,rstate)
+      results <- rbind(results,newrow)
+      
+    }
+    colnames(results) <- c("hospital", "state")
+    return(results)
+  }
+  
+  if(routcome == "pneumonia") {
+    for(rstate in states) {
+      required_state <- filter(foutcome, State == rstate)
+      #print(rstate)
+      fstate_data <- required_state[,c(2,23)]
+      tst1 <- fstate_data[order(suppressWarnings(as.numeric(fstate_data[,2])),fstate_data[,1],na.last = NA),]
+      hname <- tst1$Hospital.Name[num]
+      if(num == "worst") {
+        hname <- tst1$Hospital.Name[nrow(tst1)]
+        
+      } else if(num > nrow(tst1)) {
+        #print("setting to NA")
+        hname = "<NA>"
+      } else {
+        hname <- tst1$Hospital.Name[num]
+      }
+      newrow <- data.frame(hname,rstate)
+      results <- rbind(results,newrow)
+    
       # add to data frame
     }
     colnames(results) <- c("hospital", "state")
     return(results)
-    
   }
-  if(routcome == "heart failure") {
-    fstate_data <- foutcome[,c(2,7,17)]
-    tst1 <- fstate_data[order(suppressWarnings(as.numeric(fstate_data[,3])),fstate_data[,2],fstate_data[,1],na.last = NA),]
-    if(num == "worst") {
-      num <- nrow(tst1)
-    }
-    return(tst1$Hospital.Name[num])
-    #return(tst1)
-  }
-  if(routcome == "pneumonia") {
-    fstate_data <- required_state[,c(2,23)]
-    tst1 <- fstate_data[order(suppressWarnings(as.numeric(fstate_data[,2])),fstate_data[,1],na.last = NA),]
-    if(num == "worst") {
-      num <- nrow(tst1)
-    }
-    return(tst1$Hospital.Name[num])
-  }
-  ## Return hospital name in that state with lowest 30-day death rate
   
   
 }
